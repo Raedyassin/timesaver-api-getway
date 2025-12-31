e6abd5f4a7dc54c1ba52b5a7f24547ccfbc4327e
RaedYassin <raedyassin02468@gmail.com>
2025-12-31 16:06:14 +0200

    add some of stripe integration without test it

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,6 @@ import { ResponseInterceptor } from './common/interceptors/global-response.inter
 import { UserRoleGuard } from './common/guards/user-role.guard';
 import { SubscriptionModule } from './modules/subscription/subscription.module';
 import { PaymentModule } from './payment/payment.module';
-import { AiModule } from './modules/ai/ai.module';
 
 @Module({
   imports: [
@@ -74,7 +73,6 @@ import { AiModule } from './modules/ai/ai.module';
     PlanModule,
     SubscriptionModule,
     PaymentModule,
-    AiModule,
   ],
   controllers: [AppController],
   providers: [
